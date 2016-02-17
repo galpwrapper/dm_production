a58fc281b9094f75c9326274caa1b6c8a5606a14
chwei <weicc1989@gmail.com>
2016-02-17 15:51:47 +0100

    add zhou's spectrum move enumdef out

---
+++ anaspec_zhou/anaspec_zhou.cc
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <cstring>
+#include "TFile.h"
+#include "TGraph.h"
+
+#include "w_interp.h"
+#include "anaspec_zhou.h"
+#define ROOTNAME "DM_Spectrum.root"
+
+using std::cout;
+using std::endl;
+
+namespace zhou {
+  zhou_branch branch_map[] = {
+    e, mu, tau,
+    W, q,
+    (zhou_branch)-1, bottom, top,
+    (zhou_branch)-1,
+    four_e, four_mu, four_tau
+  };
+  zhou_product product_map[] = {
+    electron,
+    antiproton,
+    (zhou_product)-1,
+    (zhou_product)-1,
+    (zhou_product)-1
+  };
+}
+
+const bool anaspec_zhou::exist[product_choice_size][branch_choice_size] = {{1,1,1,1,0,0,1,0,0,1,1,1},
+                                                                           {0,1,0,1,1,0,1,1,0,0,0,0},
+                                                                           {0,0,0,0,0,0,0,0,0,0,0,0},
+                                                                           {0,0,0,0,0,0,0,0,0,0,0,0},
+                                                                           {0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
+anaspec_zhou::anaspec_zhou(): anaspec() {
+  memset(tg, NULL, sizeof(tg));
+  memset(loaded, false, sizeof(loaded));
+}
+
+double anaspec_zhou::ask(double E, double mdm, bool cumulate) {
+  if (cumulate) {
+    cout<<"cumulate was not usefull now"<<endl;
+    return 0;
+  }
+  double result = 0;
+  for (int i=0; i<branch_choice_size; i++) {
+    if (branch[i] && exist[product][i]) {
+      if (!loaded[product][i]) {
+        if (load(zhou::product_map[product], zhou::branch_map[i])) {
+          loaded[product][i] = true;
+        } else {
+          cout<<"[ERROR]: anaspec_zhou::ask"<<endl;
+          cout<<"         load spectrum fail"<<endl;
+          return 0;
+        }
+      }
+      result += zhou_ask(E, mdm, zhou::product_map[product],zhou::branch_map[i]);
+    }else if (branch[i] && !exist[product][i]) {
+      cout<<"[ERROR]: anaspec_zhou::ask"<<endl;
+      cout<<"         product or branch not exist"<<endl;
+    }
+  }
+  return result;
+}
+
+bool anaspec_zhou::load(zhou::zhou_product prod, zhou::zhou_branch bran) {
+#ifdef DATDIR
+  string filename = DATDIR;
+#else
+  string filename = ".";
+#endif
+  filename = filename + "/" + ROOTNAME;
+  string tgname = "G_" + zhou::branch_name[bran] + "_" + zhou::product_name[prod];
+  TFile f(filename.c_str());
+  f.GetObject(tgname.c_str(), tg[prod][bran]);
+  f.Close();
+  if (tg[prod][bran])
+    return true;
+  else
+    return false;
+}
+
+double anaspec_zhou::zhou_ask(double E, double mdm, zhou::zhou_product prod, zhou::zhou_branch bran) {
+  int n = tg[prod][bran]->GetN();
+  double mass = 0;
+  if (prod == zhou::electron) {
+    mass = eMass;
+  } else if (prod == zhou::antiproton) {
+    mass = pMass;
+  }
+  double xtemp = (E + mass) / mdm,
+         result = 0,
+         * x = tg[prod][bran]->GetX(),
+         * dndx = tg[prod][bran]->GetY();
+  if (xtemp > 1) {
+    return 0;
+  }
+  if (WMATH::interp_1D_loglog(x, dndx, n, &xtemp, &result, 1)) {
+    return result / mdm;
+  } else {
+    cout<<"[ERROR]: anaspec_zhou::zhou_ask"<<endl;
+    cout<<"         interp_1D_loglog fail"<<endl;
+    return 0;
+  }
+}
---
+++ anaspec_zhou/anaspec_zhou.h
@@ -0,0 +1,41 @@
+#ifndef _ANASPEC_zhou_H
+#define _ANASPEC_zhou_H
+#include "anaspec.h"
+#include "TGraph.h"
+
+/*********************************************************************
+This class is used to calculate generation spectrum from the
+annihilation of darkmatter Eg and mdm are both in [GeV], the result
+returned by function ask is in [GeV^-1].
+With the result of zhou
+The cumulated mode will return the value of \int_0^{E_0} dN/dE dE
+*********************************************************************/
+
+using std::string;
+
+namespace zhou{
+  enum zhou_branch {e, mu, tau, q, bottom, top, W, Z, H, ZH,
+                    four_e, four_mu, four_tau, zhou_branch_size};
+  enum zhou_product {electron, antiproton, zhou_product_size};
+
+  const string branch_name[zhou_branch_size] = {"2e", "2mu", "2tau", "2q", "2b", "2t", "2w", "2z", "2h", "zh",
+                                                "4e", "4mu", "4tau"};
+  const string product_name[zhou_product_size] = {"electron", "antiproton"};
+}
+
+class anaspec_zhou : public anaspec {
+  public:
+    anaspec_zhou();
+    double ask(double E, double mdm, bool cumulate = false);
+
+  private:
+    TGraph *tg[zhou::zhou_product_size][zhou::zhou_branch_size];
+    static const bool exist[product_choice_size][branch_choice_size];
+    bool loaded[product_choice_size][branch_choice_size];
+    static const double eMass = 0.5109990615e-3; //Electron mass in Gev
+    static const double pMass = 939.e-3; //Proton mass in Gev
+
+    bool load(zhou::zhou_product prod, zhou::zhou_branch bran);
+    double zhou_ask(double E, double mdm, zhou::zhou_product prod, zhou::zhou_branch bran);
+};
+#endif
